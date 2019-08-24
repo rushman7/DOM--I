db35238febea30c593b2a20a57900101807a7819
BuildTools <arthurpisakhov1@gmail.com>
2019-08-24 19:22:28 -0400

    nav selectors

--- js/index.js
+++ js/index.js
@@ -38,5 +38,22 @@ const siteContent = {
 };
 
 // Example: Update the img src for the logo
-let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// Nav
+let navA = document.querySelectorAll('nav a');
+
+navA[0].textContent = siteContent["nav"]["nav-item-1"]; 
+navA[1].textContent = siteContent["nav"]["nav-item-2"]; 
+navA[2].textContent = siteContent["nav"]["nav-item-3"]; 
+navA[3].textContent = siteContent["nav"]["nav-item-4"]; 
+navA[4].textContent = siteContent["nav"]["nav-item-5"]; 
+navA[5].textContent = siteContent["nav"]["nav-item-6"]; 
+
+document.getElementById("logo-img").setAttribute('src', siteContent["nav"]["img-src"]);
+
+// cta
+document.getElementById("cta-img").setAttribute('src', siteContent["cta"]["img-src"]);
+
+
+// main-content
+document.getElementById('middle-img').setAttribute('src', siteContent['main-content']['middle-img-src']);
